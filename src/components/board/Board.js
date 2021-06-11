a6716f4518e417ef88dff107c624433fa57058ea
Fabian Baumeister <fabian.baumeister@gmail.com>
2021-06-11 17:22:41 +0200

    Add Gameplay
    Clicking on column sets stones. The current player is changed.

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -5,15 +5,30 @@ class Board extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            board: new Array(6).fill(new Array(7).fill(null))
-        }
+            board: new Array(6).fill(new Array(7).fill(null)),
+            activePlayer: 'X'
+        };
     }
+
+    handleColumnClick(colIndex) {
+      for (let row = this.state.board.length - 1; row >= 0; row--) {
+        if (!this.state.board[row][colIndex]) {
+          const newField = this.state.board.map((col) => col.slice()).slice();
+          newField[row][colIndex] = this.state.activePlayer;
+          const nextPlayer = this.state.activePlayer === 'X' ? 'O': 'X';
+          this.setState({ board: newField, activePlayer: nextPlayer });
+          return;
+        }
+      }
+    }
+
     render() {
         return (
             <div><table cellSpacing="0">
                     <tbody>{ this.state.board.map((col, rowIndex) => 
                         <tr key={rowIndex}>{col.map((cellValue, colIndex) =>
-                            <td key={colIndex}> 
+                            <td key={colIndex} 
+                                onClick={() => this.handleColumnClick(colIndex)}> 
                                 {cellValue}
                             </td>
                         )}</tr>
